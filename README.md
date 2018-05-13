c6ce052797c8796e8b50cc918686a0f698e9fa2f
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-05-13 08:57:47 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# shlink-web-client
+
+[![Build Status](https://travis-ci.org/shlinkio/shlink-web-client.svg?branch=develop)](https://travis-ci.org/shlinkio/shlink-web-client)
+
+A React-based client application for [Shlink](https://shlink.io)
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
