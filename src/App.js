39a78414eb4e5a9ebf66847c2463de9a7aac74b9
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-06-03 12:57:03 +0200

    Reformated code to use 2-space indents

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.scss';
 import MainHeader from './common/MainHeader';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <MainHeader/>
-                <div className="app">
-                    <h1 className="app__title">Welcome to Shlink</h1>
-                    <h5 className="App__intro">Please, select a server.</h5>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <MainHeader/>
+        <div className="app">
+          <h1 className="app__title">Welcome to Shlink</h1>
+          <h5 className="App__intro">Please, select a server.</h5>
+        </div>
+      </div>
+    );
+  }
 }
--- src/common/MainHeader.js
+++ src/common/MainHeader.js
@@ -2,65 +2,65 @@ import plusIcon from '@fortawesome/fontawesome-free-solid/faPlus';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import React from 'react';
 import {
-    Collapse, DropdownItem, DropdownMenu,
-    DropdownToggle,
-    Nav,
-    Navbar,
-    NavbarBrand,
-    NavbarToggler,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown
+  Collapse, DropdownItem, DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown
 } from 'reactstrap';
 import './MainHeader.scss';
 import shlinkLogo from './shlink-logo-white.png';
 
 export default class MainHeader extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isOpen: false
-        };
-    }
+    this.state = {
+      isOpen: false
+    };
+  }
 
-    toggle() {
-        this.setState({
-            isOpen: ! this.state.isOpen
-        });
-    }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
-    render() {
-        return (
-            <Navbar color="primary" dark fixed="top" className="main-header" expand="md">
-                <NavbarBrand href="https://shlink.io" target="_blank">
-                    <img src={shlinkLogo} alt="Shlink" className="main-header__brand-logo"/> Shlink
-                </NavbarBrand>
-                <NavbarToggler onClick={() => this.toggle()} />
-                <Collapse navbar isOpen={this.state.isOpen}>
-                    <Nav navbar className="ml-auto">
-                        <NavItem>
-                            <NavLink href="#">
-                                <FontAwesomeIcon icon={plusIcon} />&nbsp; Add server
-                            </NavLink>
-                        </NavItem>
-                        <UncontrolledDropdown nav>
-                            <DropdownToggle nav caret>
-                                Servers
-                            </DropdownToggle>
+  render() {
+    return (
+      <Navbar color="primary" dark fixed="top" className="main-header" expand="md">
+        <NavbarBrand href="https://shlink.io" target="_blank">
+          <img src={shlinkLogo} alt="Shlink" className="main-header__brand-logo"/> Shlink
+        </NavbarBrand>
+        <NavbarToggler onClick={() => this.toggle()}/>
+        <Collapse navbar isOpen={this.state.isOpen}>
+          <Nav navbar className="ml-auto">
+            <NavItem>
+              <NavLink href="#">
+                <FontAwesomeIcon icon={plusIcon}/>&nbsp; Add server
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav>
+              <DropdownToggle nav caret>
+                Servers
+              </DropdownToggle>
 
-                            <DropdownMenu>
-                                <DropdownItem>
-                                    Server 1
-                                </DropdownItem>
-                                <DropdownItem>
-                                    Server 2
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        );
-    }
+              <DropdownMenu>
+                <DropdownItem>
+                  Server 1
+                </DropdownItem>
+                <DropdownItem>
+                  Server 2
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
 }
