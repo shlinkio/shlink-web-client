f0c20a2d1a2cda77c53d6f63daf4620aac5cc7a1
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-07-20 22:20:58 +0200

    Improved aside styles

--- src/common/AsideMenu.js
+++ src/common/AsideMenu.js
@@ -6,44 +6,41 @@ import { NavLink } from 'react-router-dom';
 import DeleteServerButton from '../servers/DeleteServerButton';
 import './AsideMenu.scss';
 
-export default class AsideMenu extends React.Component {
-  render() {
-    const { selectedServer, history } = this.props;
-    const serverId = selectedServer ? selectedServer.id : '';
-    const isListShortUrlsActive = (match, { pathname }) => {
-      // FIXME. Should use the 'match' params, but they are not being properly resolved. Investigate
-      const serverIdFromPathname = pathname.split('/')[2];
-      return serverIdFromPathname === serverId && pathname.indexOf('list-short-urls') !== -1;
-    };
+export default function AsideMenu({ selectedServer, history }) {
+  const serverId = selectedServer ? selectedServer.id : '';
+  const isListShortUrlsActive = (match, { pathname }) => {
+    // FIXME. Should use the 'match' params, but they are not being properly resolved. Investigate
+    const serverIdFromPathname = pathname.split('/')[2];
+    return serverIdFromPathname === serverId && pathname.indexOf('list-short-urls') !== -1;
+  };
 
-    return (
-      <aside className="aside-menu col-md-2 col-sm-2">
-        <nav className="nav flex-column aside-menu__nav">
-          <NavLink
-            className="aside-menu__item"
-            activeClassName="aside-menu__item--selected"
-            to={`/server/${serverId}/list-short-urls/1`}
-            isActive={isListShortUrlsActive}
-          >
-            <FontAwesomeIcon icon={listIcon} />
-            <span className="aside-menu__item-text">List short URLs</span>
-          </NavLink>
-          <NavLink
-            className="aside-menu__item"
-            activeClassName="aside-menu__item--selected"
-            to={`/server/${serverId}/create-short-url`}
-          >
-            <FontAwesomeIcon icon={createIcon} />
-            <span className="aside-menu__item-text">Create short code</span>
-          </NavLink>
-          <span className="aside-menu__item--divider" />
-          <DeleteServerButton
-            className="aside-menu__item aside-menu__item--danger"
-            history={history}
-            server={selectedServer}
-          />
-        </nav>
-      </aside>
-    );
-  }
+  return (
+    <aside className="aside-menu col-md-2 col-sm-2">
+      <nav className="nav flex-column aside-menu__nav">
+        <NavLink
+          className="aside-menu__item"
+          activeClassName="aside-menu__item--selected"
+          to={`/server/${serverId}/list-short-urls/1`}
+          isActive={isListShortUrlsActive}
+        >
+          <FontAwesomeIcon icon={listIcon} />
+          <span className="aside-menu__item-text">List short URLs</span>
+        </NavLink>
+        <NavLink
+          className="aside-menu__item"
+          activeClassName="aside-menu__item--selected"
+          to={`/server/${serverId}/create-short-url`}
+        >
+          <FontAwesomeIcon icon={createIcon} />
+          <span className="aside-menu__item-text">Create short code</span>
+        </NavLink>
+
+        <DeleteServerButton
+          className="aside-menu__item aside-menu__item--danger"
+          history={history}
+          server={selectedServer}
+        />
+      </nav>
+    </aside>
+  );
 }
