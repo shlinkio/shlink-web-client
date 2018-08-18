f1ee467af4e348e3eebb4c9b22eb8420714ab76f
Alejandro Celaya <alejandrocelaya@gmail.com>
2018-08-18 11:15:00 +0200

    Finished design on tags list page

--- src/tags/TagsList.js
+++ src/tags/TagsList.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { pick, splitEvery } from 'ramda';
 import { listTags } from './reducers/tagsList';
-import './TagsList.scss';
-import { Card } from 'reactstrap';
+import { Card, CardBody } from 'reactstrap';
 import ColorGenerator from '../utils/ColorGenerator';
 import MuttedMessage from '../utils/MuttedMessage';
+import editIcon from '@fortawesome/fontawesome-free-solid/faPencilAlt';
+import deleteIcon from '@fortawesome/fontawesome-free-solid/faTrash';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import './TagsList.scss';
 
 const { round } = Math;
 
@@ -36,15 +39,23 @@ export class TagsList extends React.Component {
         {tagsGroups.map((group, index) => (
           <div key={index} className="col-md-6 col-xl-3">
             {group.map(tag => (
-              <div
-                style={{ backgroundColor: colorGenerator.getColorForKey(tag) }}
-                className="tags-list__tag-container"
-                key={tag}
-              >
-                <Card body className="tags-list__tag-card">
-                  <h5 className="tags-list__tag-title">{tag}</h5>
-                </Card>
-              </div>
+              <Card className="tags-list__tag-card" key={tag}>
+                <CardBody className="tags-list__tag-card-body">
+                  <button className="btn btn-light btn-sm tags-list__btn">
+                    <FontAwesomeIcon icon={deleteIcon} />
+                  </button>
+                  <button className="btn btn-light btn-sm tags-list__btn">
+                    <FontAwesomeIcon icon={editIcon} />
+                  </button>
+                  <h5 className="tags-list__tag-title">
+                    <div
+                      style={{ backgroundColor: colorGenerator.getColorForKey(tag) }}
+                      className="tags-list__tag-bullet"
+                    />
+                    {tag}
+                  </h5>
+                </CardBody>
+              </Card>
             ))}
           </div>
         ))}
