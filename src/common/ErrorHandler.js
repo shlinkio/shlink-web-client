ad9f0c00d039a464506c8922e0f6d2dccc91c7c2
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-03-04 20:49:18 +0100

    Created ErrorHandler test

--- src/common/ErrorHandler.js
+++ src/common/ErrorHandler.js
@@ -14,7 +14,6 @@ const ErrorHandler = ({ location }) => class ErrorHandler extends React.Componen
   }
 
   static getDerivedStateFromError() {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
