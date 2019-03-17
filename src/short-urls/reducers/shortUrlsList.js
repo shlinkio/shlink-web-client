4a09d99322a34720175e8581988fcd3f5659a55e
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-03-17 09:11:37 +0100

    Refactored shortUrlsList to take advantage of redux-actions

--- src/short-urls/reducers/shortUrlsList.js
+++ src/short-urls/reducers/shortUrlsList.js
@@ -1,13 +1,14 @@
+import { handleActions } from 'redux-actions';
 import { assoc, assocPath, propEq, reject } from 'ramda';
 import PropTypes from 'prop-types';
 import { SHORT_URL_TAGS_EDITED } from './shortUrlTags';
 import { SHORT_URL_DELETED } from './shortUrlDeletion';
 
-/* eslint-disable padding-line-between-statements, newline-after-var */
+/* eslint-disable padding-line-between-statements */
 export const LIST_SHORT_URLS_START = 'shlink/shortUrlsList/LIST_SHORT_URLS_START';
 export const LIST_SHORT_URLS_ERROR = 'shlink/shortUrlsList/LIST_SHORT_URLS_ERROR';
 export const LIST_SHORT_URLS = 'shlink/shortUrlsList/LIST_SHORT_URLS';
-/* eslint-enable padding-line-between-statements, newline-after-var */
+/* eslint-enable padding-line-between-statements */
 
 export const shortUrlType = PropTypes.shape({
   shortCode: PropTypes.string,
@@ -22,39 +23,24 @@ const initialState = {
   error: false,
 };
 
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case LIST_SHORT_URLS_START:
-      return { ...state, loading: true, error: false };
-    case LIST_SHORT_URLS:
-      return {
-        loading: false,
-        error: false,
-        shortUrls: action.shortUrls,
-      };
-    case LIST_SHORT_URLS_ERROR:
-      return {
-        loading: false,
-        error: true,
-        shortUrls: {},
-      };
-    case SHORT_URL_TAGS_EDITED:
-      const { data } = state.shortUrls;
+export default handleActions({
+  [LIST_SHORT_URLS_START]: (state) => ({ ...state, loading: true, error: false }),
+  [LIST_SHORT_URLS]: (state, { shortUrls }) => ({ loading: false, error: false, shortUrls }),
+  [LIST_SHORT_URLS_ERROR]: () => ({ loading: false, error: true, shortUrls: {} }),
+  [SHORT_URL_TAGS_EDITED]: (state, action) => { // eslint-disable-line object-shorthand
+    const { data } = state.shortUrls;
 
-      return assocPath([ 'shortUrls', 'data' ], data.map((shortUrl) =>
-        shortUrl.shortCode === action.shortCode
-          ? assoc('tags', action.tags, shortUrl)
-          : shortUrl), state);
-    case SHORT_URL_DELETED:
-      return assocPath(
-        [ 'shortUrls', 'data' ],
-        reject(propEq('shortCode', action.shortCode), state.shortUrls.data),
-        state,
-      );
-    default:
-      return state;
-  }
-}
+    return assocPath([ 'shortUrls', 'data' ], data.map((shortUrl) =>
+      shortUrl.shortCode === action.shortCode
+        ? assoc('tags', action.tags, shortUrl)
+        : shortUrl), state);
+  },
+  [SHORT_URL_DELETED]: (state, action) => assocPath(
+    [ 'shortUrls', 'data' ],
+    reject(propEq('shortCode', action.shortCode), state.shortUrls.data),
+    state,
+  ),
+}, initialState);
 
 export const listShortUrls = (buildShlinkApiClient) => (params = {}) => async (dispatch, getState) => {
   dispatch({ type: LIST_SHORT_URLS_START });
