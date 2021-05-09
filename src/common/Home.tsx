f0849dd7d21bb2136d7811949d2e7543aeba00ec
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-05-09 14:04:35 +0200

    Edited yet again

--- src/common/Home.tsx
+++ src/common/Home.tsx
@@ -26,7 +26,7 @@ const Home = ({ servers }: HomeProps) => {
           </div>
           <div className="col-md-7 home__servers-container">
             <div className="p-4">
-              <h1 className="home__title" style={{ background: 'red' }}>Edited twice!</h1>
+              <h1 className="home__title" style={{ background: 'red' }}>Edited three times!</h1>
             </div>
             <ServersListGroup embedded servers={serversList}>
               {!hasServers && (
