55716a8f7f05fe25513598d3c4d6e587830b6869
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-06-23 19:59:06 +0200

    Created ShortUrlFormCheckboxGroup test

---
+++ test/short-urls/helpers/ShortUrlFormCheckboxGroup.test.tsx
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme';
+import { ShortUrlFormCheckboxGroup } from '../../../src/short-urls/helpers/ShortUrlFormCheckboxGroup';
+import Checkbox from '../../../src/utils/Checkbox';
+
+describe('<ShortUrlFormCheckboxGroup />', () => {
+  test.each([
+    [ undefined, '', 0 ],
+    [ 'This is the tooltip', 'mr-2', 1 ],
+  ])('renders tooltip only when provided', (infoTooltip, expectedClassName, expectedAmountOfTooltips) => {
+    const wrapper = shallow(<ShortUrlFormCheckboxGroup infoTooltip={infoTooltip} />);
+    const checkbox = wrapper.find(Checkbox);
+
+    expect(checkbox.prop('className')).toEqual(expectedClassName);
+    expect(wrapper.find('InfoTooltip')).toHaveLength(expectedAmountOfTooltips);
+  });
+});
