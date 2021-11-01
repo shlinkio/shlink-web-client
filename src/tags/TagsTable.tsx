b0c15490056de6f460b0c687ed734cb514532630
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-11-01 11:13:47 +0100

    Added sticky header to tags table

--- src/tags/TagsTable.tsx
+++ src/tags/TagsTable.tsx
@@ -8,6 +8,7 @@ import { useQueryState } from '../utils/helpers/hooks';
 import { parseQuery } from '../utils/helpers/query';
 import { TagsListChildrenProps } from './data/TagsListChildrenProps';
 import { TagsTableRowProps } from './TagsTableRow';
+import './TagsTable.scss';
 
 const TAGS_PER_PAGE = 20; // TODO Allow customizing this value in settings
 
@@ -35,11 +36,12 @@ export const TagsTable = (colorGenerator: ColorGenerator, TagsTableRow: FC<TagsT
       <table className="table table-hover mb-0">
         <thead className="responsive-table__header">
           <tr>
-            <th>Tag</th>
-            <th className="text-lg-right">Short URLs</th>
-            <th className="text-lg-right">Visits</th>
-            <th />
+            <th className="tags-table__header-cell">Tag</th>
+            <th className="tags-table__header-cell text-lg-right">Short URLs</th>
+            <th className="tags-table__header-cell text-lg-right">Visits</th>
+            <th className="tags-table__header-cell" />
           </tr>
+          <tr><th colSpan={4} className="p-0 border-top-0" /></tr>
         </thead>
         <tbody>
           {currentPage.length === 0 && <tr><td colSpan={4} className="text-center">No results found</td></tr>}
