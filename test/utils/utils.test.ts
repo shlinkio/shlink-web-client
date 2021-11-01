9cbeef1cb48d8e8984f2b4144a384b89cd4684d0
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-11-01 13:57:48 +0100

    Moved test to the right place

--- test/utils/utils.test.ts
+++ test/utils/utils.test.ts
@@ -1,29 +1,6 @@
 import { capitalize, nonEmptyValueOrNull, rangeOf } from '../../src/utils/utils';
-import { determineOrderDir } from '../../src/utils/helpers/ordering';
 
 describe('utils', () => {
-  describe('determineOrderDir', () => {
-    it('returns ASC when current order field and selected field are different', () => {
-      expect(determineOrderDir('foo', 'bar')).toEqual('ASC');
-      expect(determineOrderDir('bar', 'foo')).toEqual('ASC');
-    });
-
-    it('returns ASC when no current order dir is provided', () => {
-      expect(determineOrderDir('foo', 'foo')).toEqual('ASC');
-      expect(determineOrderDir('bar', 'bar')).toEqual('ASC');
-    });
-
-    it('returns DESC when current order field and selected field are equal and current order dir is ASC', () => {
-      expect(determineOrderDir('foo', 'foo', 'ASC')).toEqual('DESC');
-      expect(determineOrderDir('bar', 'bar', 'ASC')).toEqual('DESC');
-    });
-
-    it('returns undefined when current order field and selected field are equal and current order dir is DESC', () => {
-      expect(determineOrderDir('foo', 'foo', 'DESC')).toBeUndefined();
-      expect(determineOrderDir('bar', 'bar', 'DESC')).toBeUndefined();
-    });
-  });
-
   describe('rangeOf', () => {
     const func = (i: number) => `result_${i}`;
     const size = 5;
