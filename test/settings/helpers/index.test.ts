e954a860bf650c2d6d626b0c327431239ef1e140
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-12-23 17:59:18 +0100

    Added test for migrateDeprecatedSettings function

---
+++ test/settings/helpers/index.test.ts
@@ -0,0 +1,31 @@
+import { Mock } from 'ts-mockery';
+import { migrateDeprecatedSettings } from '../../../src/settings/helpers';
+import { ShlinkState } from '../../../src/container/types';
+
+describe('settings-helpers', () => {
+  describe('migrateDeprecatedSettings', () => {
+    it('updates settings as expected', () => {
+      const state = Mock.of<ShlinkState>({
+        settings: {
+          visits: {
+            defaultInterval: 'last180days' as any,
+          },
+          ui: {
+            tagsMode: 'list',
+          } as any,
+        },
+      });
+
+      expect(migrateDeprecatedSettings(state)).toEqual(expect.objectContaining({
+        settings: expect.objectContaining({
+          visits: {
+            defaultInterval: 'last180Days',
+          },
+          tags: {
+            defaultMode: 'list',
+          },
+        }),
+      }));
+    });
+  });
+});
