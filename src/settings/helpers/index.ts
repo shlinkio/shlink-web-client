e77b4d7a82021897b958738f75ca8780f05065ec
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-01-01 09:40:26 +0100

    Ensured settings migration function does not crash if settings are not set

--- src/settings/helpers/index.ts
+++ src/settings/helpers/index.ts
@@ -1,6 +1,10 @@
 import { ShlinkState } from '../../container/types';
 
-export const migrateDeprecatedSettings = (state: ShlinkState): ShlinkState => {
+export const migrateDeprecatedSettings = (state: Partial<ShlinkState>): Partial<ShlinkState> => {
+  if (!state.settings) {
+    return state;
+  }
+
   // The "last180Days" interval had a typo, with a lowercase d
   if ((state.settings.visits?.defaultInterval as any) === 'last180days') {
     state.settings.visits && (state.settings.visits.defaultInterval = 'last180Days');
--- test/settings/helpers/index.test.ts
+++ test/settings/helpers/index.test.ts
@@ -4,6 +4,10 @@ import { ShlinkState } from '../../../src/container/types';
 
 describe('settings-helpers', () => {
   describe('migrateDeprecatedSettings', () => {
+    it('returns object as is if settings are not set', () => {
+      expect(migrateDeprecatedSettings({})).toEqual({});
+    });
+
     it('updates settings as expected', () => {
       const state = Mock.of<ShlinkState>({
         settings: {
