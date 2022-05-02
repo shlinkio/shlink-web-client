e8a5eadd2beb4ca5aac2c3cfd4c2539da8c9ff54
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-05-02 19:34:33 +0200

    Migrated ShlinkApiError test from enzyme to react testing library

--- test/api/ShlinkApiError.test.tsx
+++ test/api/ShlinkApiError.test.tsx
@@ -1,26 +1,20 @@
-import { shallow, ShallowWrapper } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { Mock } from 'ts-mockery';
 import { ShlinkApiError, ShlinkApiErrorProps } from '../../src/api/ShlinkApiError';
 import { InvalidArgumentError, ProblemDetailsError } from '../../src/api/types';
 
 describe('<ShlinkApiError />', () => {
-  let commonWrapper: ShallowWrapper;
-  const createWrapper = (props: ShlinkApiErrorProps) => {
-    commonWrapper = shallow(<ShlinkApiError {...props} />);
-
-    return commonWrapper;
-  };
-
-  afterEach(() => commonWrapper?.unmount());
+  const setUp = (props: ShlinkApiErrorProps) => render(<ShlinkApiError {...props} />);
 
   it.each([
     [undefined, 'the fallback', 'the fallback'],
     [Mock.all<ProblemDetailsError>(), 'the fallback', 'the fallback'],
     [Mock.of<ProblemDetailsError>({ detail: 'the detail' }), 'the fallback', 'the detail'],
   ])('renders proper message', (errorData, fallbackMessage, expectedMessage) => {
-    const wrapper = createWrapper({ errorData, fallbackMessage });
+    const { container } = setUp({ errorData, fallbackMessage });
 
-    expect(wrapper.text()).toContain(expectedMessage);
+    expect(container.firstChild).toHaveTextContent(expectedMessage);
+    expect(screen.queryByRole('paragraph')).not.toBeInTheDocument();
   });
 
   it.each([
@@ -28,9 +22,7 @@ describe('<ShlinkApiError />', () => {
     [Mock.all<ProblemDetailsError>(), 0],
     [Mock.of<InvalidArgumentError>({ type: 'INVALID_ARGUMENT', invalidElements: [] }), 1],
   ])('renders list of invalid elements when provided error is an InvalidError', (errorData, expectedElementsCount) => {
-    const wrapper = createWrapper({ errorData });
-    const p = wrapper.find('p');
-
-    expect(p).toHaveLength(expectedElementsCount);
+    setUp({ errorData });
+    expect(screen.queryAllByText(/^Invalid elements/)).toHaveLength(expectedElementsCount);
   });
 });
