f72251c125a74d48e3ce1fa68698b058adb3987b
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-05-03 17:49:42 +0200

    Migrated AppUpdateBanner test to react testing library

--- src/common/AppUpdateBanner.tsx
+++ src/common/AppUpdateBanner.tsx
@@ -24,7 +24,7 @@ export const AppUpdateBanner: FC<AppUpdateBannerProps> = ({ isOpen, toggle, forc
       <h4 className="mb-4">This app has just been updated!</h4>
       <p className="mb-0">
         Restart it to enjoy the new features.
-        <Button disabled={isUpdating} className="ms-2" color="secondary" size="sm" onClick={update}>
+        <Button role="button" disabled={isUpdating} className="ms-2" color="secondary" size="sm" onClick={update}>
           {!isUpdating && <>Restart now <FontAwesomeIcon icon={reloadIcon} className="ms-1" /></>}
           {isUpdating && <>Restarting...</>}
         </Button>
