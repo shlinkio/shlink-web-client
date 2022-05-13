64ee9a39cc6c31fc031b58945943d0ce10425160
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-05-13 20:18:40 +0200

    Migrated HorizontalBarChart test to react testing library

--- test/visits/charts/HorizontalBarChart.test.tsx
+++ test/visits/charts/HorizontalBarChart.test.tsx
@@ -1,18 +1,15 @@
-import { render } from '@testing-library/react';
 import { HorizontalBarChart, HorizontalBarChartProps } from '../../../src/visits/charts/HorizontalBarChart';
+import { setUpCanvas } from '../../__mocks__/setUpCanvas';
 
 describe('<HorizontalBarChart />', () => {
-  const setUp = (props: HorizontalBarChartProps) => {
-    const { container } = render(<HorizontalBarChart {...props} />);
-    return container.querySelector('canvas')?.getContext('2d')?.__getEvents(); // eslint-disable-line no-underscore-dangle
-  };
+  const setUp = (props: HorizontalBarChartProps) => setUpCanvas(<HorizontalBarChart {...props} />);
 
   it.each([
     [{ foo: 123, bar: 456 }, undefined],
     [{ one: 999, two: 131313 }, { one: 30, two: 100 }],
     [{ one: 999, two: 131313, max: 3 }, { one: 30, two: 100 }],
   ])('renders chart with expected canvas', (stats, highlightedStats) => {
-    const events = setUp({ stats, highlightedStats });
+    const { events } = setUp({ stats, highlightedStats });
 
     expect(events).toBeTruthy();
     expect(events).toMatchSnapshot();
