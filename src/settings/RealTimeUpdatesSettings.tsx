a90c3da7b6f4e9357272325a668e573a9917bb1e
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-06-06 22:23:21 +0200

    Migrated RealTimeUpdatesSettings test to react testing library

--- src/settings/RealTimeUpdatesSettings.tsx
+++ src/settings/RealTimeUpdatesSettings.tsx
@@ -5,6 +5,7 @@ import { SimpleCard } from '../utils/SimpleCard';
 import { FormText } from '../utils/forms/FormText';
 import { LabeledFormGroup } from '../utils/forms/LabeledFormGroup';
 import { Settings } from './reducers/settings';
+import { useDomId } from '../utils/helpers/hooks';
 
 interface RealTimeUpdatesProps {
   settings: Settings;
@@ -16,39 +17,46 @@ const intervalValue = (interval?: number) => (!interval ? '' : `${interval}`);
 
 export const RealTimeUpdatesSettings = (
   { settings: { realTimeUpdates }, toggleRealTimeUpdates, setRealTimeUpdatesInterval }: RealTimeUpdatesProps,
-) => (
-  <SimpleCard title="Real-time updates" className="h-100">
-    <FormGroup>
-      <ToggleSwitch checked={realTimeUpdates.enabled} onChange={toggleRealTimeUpdates}>
-        Enable or disable real-time updates.
-        <FormText>
-          Real-time updates are currently being <b>{realTimeUpdates.enabled ? 'processed' : 'ignored'}</b>.
-        </FormText>
-      </ToggleSwitch>
-    </FormGroup>
-    <LabeledFormGroup
-      noMargin
-      label="Real-time updates frequency (in minutes):"
-      labelClassName={classNames('form-label', { 'text-muted': !realTimeUpdates.enabled })}
-    >
-      <Input
-        type="number"
-        min={0}
-        placeholder="Immediate"
-        disabled={!realTimeUpdates.enabled}
-        value={intervalValue(realTimeUpdates.interval)}
-        onChange={({ target }) => setRealTimeUpdatesInterval(Number(target.value))}
-      />
-      {realTimeUpdates.enabled && (
-        <FormText>
-          {realTimeUpdates.interval !== undefined && realTimeUpdates.interval > 0 && (
-            <span>
-              Updates will be reflected in the UI every <b>{realTimeUpdates.interval}</b> minute{realTimeUpdates.interval > 1 && 's'}.
-            </span>
-          )}
-          {!realTimeUpdates.interval && 'Updates will be reflected in the UI as soon as they happen.'}
-        </FormText>
-      )}
-    </LabeledFormGroup>
-  </SimpleCard>
-);
+) => {
+  const inputId = useDomId();
+
+  return (
+    <SimpleCard title="Real-time updates" className="h-100">
+      <FormGroup>
+        <ToggleSwitch checked={realTimeUpdates.enabled} onChange={toggleRealTimeUpdates}>
+          Enable or disable real-time updates.
+          <FormText>
+            Real-time updates are currently being <b>{realTimeUpdates.enabled ? 'processed' : 'ignored'}</b>.
+          </FormText>
+        </ToggleSwitch>
+      </FormGroup>
+      <LabeledFormGroup
+        noMargin
+        label="Real-time updates frequency (in minutes):"
+        labelClassName={classNames('form-label', { 'text-muted': !realTimeUpdates.enabled })}
+        id={inputId}
+      >
+        <Input
+          type="number"
+          min={0}
+          placeholder="Immediate"
+          disabled={!realTimeUpdates.enabled}
+          value={intervalValue(realTimeUpdates.interval)}
+          id={inputId}
+          onChange={({ target }) => setRealTimeUpdatesInterval(Number(target.value))}
+        />
+        {realTimeUpdates.enabled && (
+          <FormText>
+            {realTimeUpdates.interval !== undefined && realTimeUpdates.interval > 0 && (
+              <span>
+                Updates will be reflected in the UI
+                every <b>{realTimeUpdates.interval}</b> minute{realTimeUpdates.interval > 1 && 's'}.
+              </span>
+            )}
+            {!realTimeUpdates.interval && 'Updates will be reflected in the UI as soon as they happen.'}
+          </FormText>
+        )}
+      </LabeledFormGroup>
+    </SimpleCard>
+  );
+};
