79645099ba157fca6efb61ca0da47ed08e711f34
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-11-03 20:53:59 +0100

    Added explicit import

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,4 @@
-import Bottle from 'bottlejs';
+import { IContainer } from 'bottlejs';
 import { combineReducers } from 'redux';
 import serversReducer from '../servers/reducers/servers';
 import selectedServerReducer from '../servers/reducers/selectedServer';
@@ -22,7 +22,7 @@ import appUpdatesReducer from '../app/reducers/appUpdates';
 import sidebarReducer from '../common/reducers/sidebar';
 import { ShlinkState } from '../container/types';
 
-export default (container: Bottle.IContainer) => combineReducers<ShlinkState>({
+export default (container: IContainer) => combineReducers<ShlinkState>({
   servers: serversReducer,
   selectedServer: selectedServerReducer,
   shortUrlsList: shortUrlsListReducer,
