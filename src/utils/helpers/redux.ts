7add83f9856f2dd3d85a161b2df475e75f73458c
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-11-13 18:26:35 +0100

    Changed how errors are serialized by async thunks

--- src/utils/helpers/redux.ts
+++ src/utils/helpers/redux.ts
@@ -1,7 +1,12 @@
 import { createAsyncThunk as baseCreateAsyncThunk, AsyncThunkPayloadCreator } from '@reduxjs/toolkit';
+import { identity } from 'ramda';
 import { ShlinkState } from '../../container/types';
 
 export const createAsyncThunk = <Returned, ThunkArg>(
   typePrefix: string,
-  payloadCreator: AsyncThunkPayloadCreator<Returned, ThunkArg, { state: ShlinkState }>,
-) => baseCreateAsyncThunk(typePrefix, payloadCreator);
+  payloadCreator: AsyncThunkPayloadCreator<Returned, ThunkArg, { state: ShlinkState, serializedErrorType: any }>,
+) => baseCreateAsyncThunk(
+    typePrefix,
+    payloadCreator,
+    { serializeError: identity },
+  );
