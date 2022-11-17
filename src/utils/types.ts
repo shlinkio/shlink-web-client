b8adf5f2747d7774d34bf600cfdb3f6ef6634e35
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-11-17 21:30:42 +0100

    Created test for HttpClient

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,3 @@
 export type MediaMatcher = (query: string) => MediaQueryList;
 
 export type Fetch = typeof window.fetch;
-
-export type JsonFetch = <T>(url: string, options?: RequestInit) => Promise<T>;
