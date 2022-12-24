ced3fa00ef784cee64c58c8737d7e9c27f14bef4
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-12-24 18:20:01 +0100

    Ensured service-worker is bundled separately

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,19 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
+    rollupOptions: {
+      input: {
+        index: './index.html', // default entry point
+        'service-worker': './src/service-worker.ts', // service worker
+      },
+      output: {
+        entryFileNames: ({ name }) => (
+          name === 'service-worker'
+            ? '[name].js' // put service worker in root
+            : 'assets/[name]-[hash].js' // others in `assets/js/`
+        ),
+      },
+    },
   },
   server: {
     port: 3000,
