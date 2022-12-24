52f556eb2ee6c78235ee7f2f90f53f90e5e3cdb6
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-12-24 10:18:26 +0100

    Migrated from react-scripts and webpack to vite

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    port: 3000,
+  },
+});
