a0cb0d0e80587468994a5f73c0d66e874d4471af
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-12-31 10:51:17 +0100

    Added chunk-splitting plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 import { manifest } from './manifest';
 import pack from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
+  plugins: [react(), chunkSplitPlugin(), VitePWA({
     strategies: 'injectManifest',
     srcDir: './src',
     filename: 'service-worker.ts',
