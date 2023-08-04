bdcfcee60e52a6599d402aba378498a25ffafa12
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-08-04 09:24:21 +0200

    Fix incorrect import in shlink-frontend-kit test

--- shlink-frontend-kit/test/__helpers__/setUpTest.ts
+++ shlink-frontend-kit/test/__helpers__/setUpTest.ts
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event/index';
+import userEvent from '@testing-library/user-event';
 import type { ReactElement } from 'react';
 
 export const renderWithEvents = (element: ReactElement) => ({
