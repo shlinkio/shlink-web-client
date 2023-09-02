1ef96662a332e9a2f8133fb583ebaeae40fce983
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-09-02 10:48:02 +0200

    Make DeleteServerButton more accessible

--- src/servers/DeleteServerButton.tsx
+++ src/servers/DeleteServerButton.tsx
@@ -1,6 +1,7 @@
 import { faMinusCircle as deleteIcon } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useToggle } from '@shlinkio/shlink-frontend-kit';
+import classNames from 'classnames';
 import type { FC, PropsWithChildren } from 'react';
 import type { ServerWithId } from './data';
 import type { DeleteServerModalProps } from './DeleteServerModal';
@@ -18,10 +19,10 @@ export const DeleteServerButton = (DeleteServerModal: FC<DeleteServerModalProps>
 
   return (
     <>
-      <span className={className} onPointerDown={showModal}>
+      <button type="button" className={classNames(className, 'p-0 bg-transparent border-0')} onClick={showModal}>
         {!children && <FontAwesomeIcon fixedWidth icon={deleteIcon} />}
         <span className={textClassName}>{children ?? 'Remove this server'}</span>
-      </span>
+      </button>
 
       <DeleteServerModal server={server} isOpen={isModalOpen} toggle={hideModal} />
     </>
