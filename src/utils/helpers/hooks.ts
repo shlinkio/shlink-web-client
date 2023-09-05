0a77df9d406ec8e59f0034437bada6c36aa76b18
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-09-05 22:56:11 +0200

    Fix wrong definition of custom hooks

--- src/utils/helpers/hooks.ts
+++ src/utils/helpers/hooks.ts
@@ -1,12 +1,13 @@
 import { parseQuery } from '@shlinkio/shlink-frontend-kit';
+import { useCallback, useMemo } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 export const useGoBack = () => {
   const navigate = useNavigate();
-  return () => navigate(-1);
+  return useCallback(() => navigate(-1), [navigate]);
 };
 
 export const useParsedQuery = <T>(): T => {
   const { search } = useLocation();
-  return parseQuery<T>(search);
+  return useMemo(() => parseQuery<T>(search), [search]);
 };
