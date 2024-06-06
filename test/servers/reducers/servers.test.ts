b2908d2b1fba8efa5036ba558e753582291dce85
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-06-06 17:49:12 +0200

    Update to JS coding standard v2.5.0

--- test/servers/reducers/servers.test.ts
+++ test/servers/reducers/servers.test.ts
@@ -107,7 +107,9 @@ describe('serversReducer', () => {
       });
 
       it('generates an id for every provided server if they do not have it', () => {
-        const servers = Object.values(list).map(({ id, ...rest }) => rest);
+        const servers = Object.values(list).map(({ name, autoConnect, url, apiKey }) => (
+          { name, autoConnect, url, apiKey }
+        ));
         const { payload } = createServers(servers);
 
         expect(Object.values(payload).every(({ id }) => !!id)).toEqual(true);
