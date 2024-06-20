f530fb4ceb4151cab51b736d604bcd7e02059f88
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-06-20 14:56:32 +0200

    Update coding standard to v3.0.0

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+import shlink from '@shlinkio/eslint-config-js-coding-standard';
+
+/* eslint-disable-next-line no-restricted-exports */
+export default shlink;
--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -9,14 +9,14 @@
 
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://cra.link/PWA
-import pack from'../package.json';
+import pack from '../package.json';
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 
 type Config = {
@@ -47,7 +47,7 @@ export function register(config?: Config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://cra.link/PWA'
+              'worker. To learn more, visit https://cra.link/PWA',
           );
         });
       } else {
@@ -75,7 +75,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // content until all client tabs are closed.
               console.log(
                 'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://cra.link/PWA.'
+                  'tabs for this page are closed. See https://cra.link/PWA.',
               );
 
               // Execute callback
