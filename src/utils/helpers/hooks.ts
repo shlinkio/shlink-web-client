fa10aebeab373948d1bd2c1ee09046d4fb92435a
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-07-22 18:33:35 +0200

    Update to latest frontend-kit

--- src/utils/helpers/hooks.ts
+++ src/utils/helpers/hooks.ts
@@ -1,13 +1,7 @@
-import { parseQuery } from '@shlinkio/shlink-frontend-kit';
-import { useCallback, useMemo } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const useGoBack = () => {
   const navigate = useNavigate();
   return useCallback(() => navigate(-1), [navigate]);
-};
-
-export const useParsedQuery = <T>(): T => {
-  const { search } = useLocation();
-  return useMemo(() => parseQuery<T>(search), [search]);
 };
