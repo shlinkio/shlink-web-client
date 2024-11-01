9b891c83faa54124087499e8e04960a97d611758
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-11-01 12:11:12 +0100

    Delete unused FormText component

--- src/utils/forms/FormText.tsx
+++
@@ -1,5 +0,0 @@
-import type { FC, PropsWithChildren } from 'react';
-
-export const FormText: FC<PropsWithChildren<unknown>> = ({ children }) => (
-  <small className="form-text text-muted d-block">{children}</small>
-);
