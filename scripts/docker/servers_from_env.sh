8da630e14972b205474772cac936eba73804a274
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-04-20 13:22:35 +0200

    Support SHLINK_SERVER_FORWARD_CREDENTIALS in docker image

--- scripts/docker/servers_from_env.sh
+++ scripts/docker/servers_from_env.sh
@@ -10,7 +10,8 @@ setup_single_shlink_server() {
   [ -n "$SHLINK_SERVER_URL" ] || return 0
   [ -n "$SHLINK_SERVER_API_KEY" ] || return 0
   local name="${SHLINK_SERVER_NAME:-Shlink}"
-  echo "[{\"name\":\"${name}\",\"url\":\"${SHLINK_SERVER_URL}\",\"apiKey\":\"${SHLINK_SERVER_API_KEY}\"}]" > /usr/share/nginx/html/servers.json
+  local forwardCredentials="${SHLINK_SERVER_FORWARD_CREDENTIALS:-false}"
+  echo "[{\"name\":\"${name}\",\"url\":\"${SHLINK_SERVER_URL}\",\"apiKey\":\"${SHLINK_SERVER_API_KEY}\",\"forwardCredentials\":${forwardCredentials}}]" > /usr/share/nginx/html/servers.json
 }
 
 setup_single_shlink_server
